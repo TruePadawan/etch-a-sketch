44c79f9c3d31f4c37522702e21a546f849b8028e
Hermes <hermeschigoziri@gmail.com>
2021-12-20 16:58:07 +0100

    Add an Eraser button

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let container = document.querySelector('.container');
+let eraser = document.getElementById('eraser');
+
+function generateRandomColor()
+{
+  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  return `rgb(${r},${g},${b})`;
+}
 
 function clearGrid()
 {
@@ -26,20 +36,46 @@ function generateGridLayout(num)
       gridUnit.style.width = `calc(100%/${num})`;
       gridUnit.style.height = `calc(100%/${num})`;
       gridUnit.addEventListener("mouseenter", () => {
-        gridUnit.style.backgroundColor = generateRandomColor();
+        gridUnit.style.backgroundColor = "black";
       });
       container.appendChild(gridUnit);
     }
   }
 }
 
-function generateRandomColor()
+function eraserHandler()
 {
-  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-  const r = randomBetween(0, 255);
-  const g = randomBetween(0, 255);
-  const b = randomBetween(0, 255);
-  return `rgb(${r},${g},${b})`;
+  if (eraser.dataset.active == "off")
+  {
+    eraser.style.backgroundColor = "green";
+    eraser.dataset.active = "on";
+
+    for (let i = 0; i < container.children.length; ++i)
+    {
+      container.children[i].removeEventListener("mouseenter", () => {
+        gridUnit.style.backgroundColor = "black";
+      });
+      container.children[i].addEventListener("mouseenter", () => {
+        container.children[i].style.backgroundColor = "white";
+      });
+    }
+
+  }
+  else
+  {
+    eraser.style.backgroundColor = "transparent";
+    eraser.dataset.active = "off";
+
+    for (let i = 0; i < container.children.length; ++i)
+    {
+      container.children[i].removeEventListener("mouseenter", () => {
+        gridUnit.style.backgroundColor = "white";
+      });
+      container.children[i].addEventListener("mouseenter", () => {
+        container.children[i].style.backgroundColor = "black";
+      });
+    }
+  }
 }
 
 generateGridLayout(16);
